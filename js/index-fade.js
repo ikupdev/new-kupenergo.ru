4036f532dbe913431d50e8de4c57fa83543c80e1
Ilya Kupriyanov <ikupdev@gmail.com>
2020-09-06 12:13:12 +0300

    fade-in effect added to pages

---
+++ js/index-fade.js
@@ -0,0 +1,9 @@
+$(window).ready(function () {
+    $('.navbar').fadeIn(200);
+    $('.simple-slider').fadeIn(400);
+    $('.company-features').fadeIn(500);
+    $('.fields').fadeIn(600);
+    $('#content').fadeIn(700);
+    $('.footer').fadeIn(800);
+    $('.benefits').fadeIn(900);
+});
---
+++ js/projects-fade.js
@@ -0,0 +1,5 @@
+$(window).ready(function () {
+    $('.navbar').fadeIn(200);
+    $('.page-contacts').fadeIn(500);
+    $('.footer').fadeIn(800);
+});
